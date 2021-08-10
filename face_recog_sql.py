a104197a67593df2c6c90049b46c9b4da433555c
veeravalli-bindu-sree <64535926+veeravalli-bindu-sree@users.noreply.github.com>
2021-08-10 22:39:15 +0530

    Add files via upload

---
+++ face_recog_sql.py
@@ -0,0 +1,114 @@
+import pymysql
+conn = pymysql.connect(
+    host='localhost',
+    user='root', 
+    password = "",
+    db='bindu',
+    )
+cur = conn.cursor()
+cur.execute("select * from stdt")
+output = cur.fetchall()
+details = []
+for i in range(len(output)):
+    details.append(output[i])
+print(details)
+# To close the connection
+conn.close()
+
+def details(name):
+    for i in range(len(details)):
+        if name in details[i][0]:
+            return details[i]
+        else:
+            return "not found"
+
+from flask import Flask, render_template, Response
+import cv2
+import face_recognition
+import numpy as np
+app=Flask(__name__)
+camera = cv2.VideoCapture(0)
+# Load a sample picture and learn how to recognize it.
+charan_image = face_recognition.load_image_file("charan.jpeg")
+charan_face_encoding = face_recognition.face_encodings(charan_image)[0]
+
+# Load a second sample picture and learn how to recognize it.
+bindu_image = face_recognition.load_image_file("bindu.jpeg")
+bindu_face_encoding = face_recognition.face_encodings(bindu_image)[0]
+
+# Create arrays of known face encodings and their names
+known_face_encodings = [
+    charan_face_encoding,
+    bindu_face_encoding
+]
+known_face_names = [
+    "charan",
+    "bindu"
+]
+# Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+
+def gen_frames():  
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            # Resize frame of video to 1/4 size for faster face recognition processing
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+            rgb_small_frame = small_frame[:, :, ::-1]
+
+            # Only process every other frame of video to save time
+           
+            # Find all the faces and face encodings in the current frame of video
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+            face_names = []
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face(s)
+                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+                name = "Unknown"
+                # Or instead, use the known face with the smallest distance to the new face
+                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = known_face_names[best_match_index]
+                    name = details(name)
+
+                face_names.append(name)
+                
+
+            # Display the results
+            for (top, right, bottom, left), name in zip(face_locations, face_names):
+                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+                top *= 4
+                right *= 4
+                bottom *= 4
+                left *= 4
+
+                # Draw a box around the face
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+                # Draw a label with a name below the face
+                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+                font = cv2.FONT_HERSHEY_DUPLEX
+                cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+if __name__=='__main__':
+    app.run(port=5002,debug=True)
+    del camera
